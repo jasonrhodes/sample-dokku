450e4ced93fb17046b4ab16de1ad267183f6d83f
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2016-04-04 07:33:43 -0400

    Added env var route

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ app.get('/', function(req, res) {
   res.send('You found my sweet dokku app');
 });
 
+app.get('/env', function(req, res) {
+  if (process.env.MY_PRIVATE_VAR) {
+    res.send(`shh you found the private env var ${process.env.MY_PRIVATE_VAR}`);
+  } else {
+    res.send('To set a private env var in dokku, log into the dokku box and use "$ dokku config:set <your-app> MY_PRIVATE_VAR=yaysocool"');
+  }
+});
+
 app.listen(process.env.PORT || 9999, function() {
   console.log(`App listening on port ${process.env.PORT}`);
 });
